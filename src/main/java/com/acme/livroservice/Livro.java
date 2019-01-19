d6a163d1330a7973f2b576d75700703b245a9070
tiago <tiagolpadua@gmail.com>
2019-01-19 21:30:05 -0200

    Introdução

---
+++ src/main/java/com/acme/livroservice/Livro.java
@@ -0,0 +1,53 @@
+package com.acme.livroservice;
+
+public class Livro {
+	private Long id;
+	private String autor;
+	private String titulo;
+	private Double preco;
+	
+	public Livro() {
+		super();
+	}
+	
+	public Livro(Long id, String autor, String titulo, Double preco) {
+		super();
+		this.id = id;
+		this.autor = autor;
+		this.titulo = titulo;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
---
+++ src/main/java/com/acme/livroservice/LivrosController.java
@@ -0,0 +1,26 @@
+package com.acme.livroservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LivrosController {
+
+	@RequestMapping("/livros")
+	public List<Livro> getLivros() {
+		ArrayList<Livro> livros = new ArrayList<Livro>();
+		
+		Livro l1 = new Livro(1l, "Don Quixote", "Miguel de Cervantes", 144.0);
+		Livro l2 = new Livro(2l, "O Senhor dos Anéis", "J. R. R. Tolkien", 123.0);
+		Livro l3 = new Livro(3l, "O Pequeno Príncipe", "Antoine de Saint-Exupéry", 152.0);
+		
+		livros.add(l1);
+		livros.add(l2);
+		livros.add(l3);
+		
+		return livros;
+	}
+}
