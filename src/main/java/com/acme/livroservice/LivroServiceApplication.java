200b75c69da9c28ca4db114cfdc676be8a6e783f
tiago <tiagolpadua@gmail.com>
2019-01-19 23:30:50 -0200

    get por id

--- src/main/java/com/acme/livroservice/LivroServiceApplication.java
+++ src/main/java/com/acme/livroservice/LivroServiceApplication.java
@@ -1,10 +1,31 @@
 package com.acme.livroservice;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LivroServiceApplication {
+	
+	@Bean
+	public ArrayList<Livro> listaLivros() {
+		
+		ArrayList<Livro> livros = new ArrayList<Livro>();
+		
+		Livro l1 = new Livro(1l, "Don Quixote", "Miguel de Cervantes", 144.0);
+		Livro l2 = new Livro(2l, "O Senhor dos Anéis", "J. R. R. Tolkien", 123.0);
+		Livro l3 = new Livro(3l, "O Pequeno Príncipe", "Antoine de Saint-Exupéry", 152.0);
+		Livro l4 = new Livro(4l, "Um Conto de Duas Cidades", "Charles Dickens", 35.0);
+		
+		livros.add(l1);
+		livros.add(l2);
+		livros.add(l3);
+		livros.add(l4);
+		
+		return livros;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivroServiceApplication.class, args);
--- src/main/java/com/acme/livroservice/LivrosController.java
+++ src/main/java/com/acme/livroservice/LivrosController.java
@@ -3,6 +3,8 @@ package com.acme.livroservice;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,25 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LivrosController {
 
+	@Resource
+	private ArrayList<Livro> listaLivros;
+
 	@RequestMapping("/livros")
 	public List<Livro> getLivros() {
-		ArrayList<Livro> livros = new ArrayList<Livro>();
-		
-		Livro l1 = new Livro(1l, "Don Quixote", "Miguel de Cervantes", 144.0);
-		Livro l2 = new Livro(2l, "O Senhor dos Anéis", "J. R. R. Tolkien", 123.0);
-		Livro l3 = new Livro(3l, "O Pequeno Príncipe", "Antoine de Saint-Exupéry", 152.0);
-		
-		livros.add(l1);
-		livros.add(l2);
-		livros.add(l3);
-		
-		return livros;
+		return listaLivros;
 	}
-	
+
 	@RequestMapping("/livros/{livroId}")
 	public Livro getLivroPorId(@PathVariable Long livroId) {
-		System.out.println("livroId: " + livroId);
-		Livro l = new Livro(1l, "Don Quixote", "Miguel de Cervantes", 144.0);
-		return l;
+		return listaLivros.stream().filter(l -> l.getId().equals(livroId)).findFirst().orElse(null);
 	}
 }
