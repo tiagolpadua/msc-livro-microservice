b50c98e4b03890400f84b149e423a03c92af9f7c
tiago <tiagolpadua@gmail.com>
2019-01-20 19:03:38 -0200

    Pesquisando

--- src/main/java/com/acme/livroservice/LivroServiceApplication.java
+++ src/main/java/com/acme/livroservice/LivroServiceApplication.java
@@ -14,10 +14,10 @@ public class LivroServiceApplication {
 		
 		ArrayList<Livro> livros = new ArrayList<Livro>();
 		
-		Livro l1 = new Livro(1l, "Don Quixote", "Miguel de Cervantes", 144.0);
-		Livro l2 = new Livro(2l, "O Senhor dos Anéis", "J. R. R. Tolkien", 123.0);
-		Livro l3 = new Livro(3l, "O Pequeno Príncipe", "Antoine de Saint-Exupéry", 152.0);
-		Livro l4 = new Livro(4l, "Um Conto de Duas Cidades", "Charles Dickens", 35.0);
+		Livro l1 = new Livro(1l, "Miguel de Cervantes", "Don Quixote", 144.0);
+		Livro l2 = new Livro(2l, "J. R. R. Tolkien", "O Senhor dos Anéis", 123.0);
+		Livro l3 = new Livro(3l, "Antoine de Saint-Exupéry", "O Pequeno Príncipe", 152.0);
+		Livro l4 = new Livro(4l, "Charles Dickens", "Um Conto de Duas Cidades", 35.0);
 		
 		livros.add(l1);
 		livros.add(l2);
--- src/main/java/com/acme/livroservice/LivrosController.java
+++ src/main/java/com/acme/livroservice/LivrosController.java
@@ -2,6 +2,8 @@ package com.acme.livroservice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,16 +25,32 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/livros")
 public class LivrosController {
-	
+
 	Logger logger = LoggerFactory.getLogger(LivrosController.class);
 
 	@Resource
 	private ArrayList<Livro> listaLivros;
 
 	@GetMapping
-	public List<Livro> getLivros() {
-		logger.info("getLivros");
-		return listaLivros;
+	public List<Livro> getLivros(@RequestParam("autor") Optional<String> autor,
+			@RequestParam("titulo") Optional<String> titulo) {
+		logger.info("getLivros - autor: " + autor.orElse("Não informado") + " titulo: " + titulo.orElse("Não informado"));
+
+		List<Livro> listaRetorno = listaLivros;
+
+		if (autor.isPresent()) {
+			listaRetorno = listaRetorno.stream()
+					.filter(l -> l.getAutor().toUpperCase().contains(autor.get().toUpperCase()))
+					.collect(Collectors.toList());
+		}
+		
+		if (titulo.isPresent()) {
+			listaRetorno = listaRetorno.stream()
+					.filter(l -> l.getTitulo().toUpperCase().contains(titulo.get().toUpperCase()))
+					.collect(Collectors.toList());
+		}
+
+		return listaRetorno;
 	}
 
 	@GetMapping("/{id}")
@@ -48,32 +67,32 @@ public class LivrosController {
 		Long max = listaLivros.stream().mapToLong(l -> l.getId()).max().orElse(0);
 		livro.setId(max + 1);
 		listaLivros.add(livro);
-		
+
 		logger.info("adicionarLivro: " + livro + " adicionado com sucesso");
 		return livro;
 	}
-	
+
 	@PutMapping("/{id}")
 	public Livro atualizarLivro(@RequestBody Livro livro, @PathVariable Long id) {
 		logger.info("atualizarLivro: " + livro + " id: " + id);
 		Livro livroSalvo = listaLivros.stream().filter(l -> l.getId().equals(id)).findFirst()
-			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Livro não encontrado: " + id));
-		
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Livro não encontrado: " + id));
+
 		livroSalvo.setAutor(livro.getAutor());
 		livroSalvo.setPreco(livro.getPreco());
 		livroSalvo.setTitulo(livro.getTitulo());
 
 		return livroSalvo;
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void excluirLivro(@PathVariable Long id) {
 		logger.info("excluirLivro: " + id);
-		
+
 		Livro livro = listaLivros.stream().filter(l -> l.getId().equals(id)).findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Livro não encontrado: " + id));
-		
+
 		listaLivros.remove(livro);
 	}
 }
