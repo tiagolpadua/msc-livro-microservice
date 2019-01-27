9ba3910b7359c2277f945bb5fbe4d9349c331cf8
tiago <tiagolpadua@gmail.com>
2019-01-27 00:32:23 -0200

    consul e actuator

--- src/main/java/com/acme/livroservice/LivroServiceApplication.java
+++ src/main/java/com/acme/livroservice/LivroServiceApplication.java
@@ -10,9 +10,11 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LivroServiceApplication {
 
 	static final String TOPIC_EXCHANGE_NAME = "livro-service-exchange";
